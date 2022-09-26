758f42dab238f0dd0760c5519b11674fda470401
shubh-dave <shubh.d@simformsolutions.com>
2022-09-26 17:49:57 +0530

    Post structure

--- components/Post.js
+++ components/Post.js
@@ -4,7 +4,13 @@ import Image from "next/image";
 function Post({ post }) {
   return (
     <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
-      <Image src={post} alt="" height={420} width={600} />
+      {console.log(post)}
+      <Image
+        src={post.frontmatter.cover_image}
+        alt=""
+        height={420}
+        width={600}
+      />
     </div>
   );
 }
