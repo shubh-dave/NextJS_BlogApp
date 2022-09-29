b2c39ea189b746485a3ab9306aa1a3365b4130bc
shubh-dave <shubh.d@simformsolutions.com>
2022-09-29 13:03:43 +0530

    Search Component

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,28 @@
-import Head from 'next/head'
-import React from 'react'
-import Header from './Header'
+import Head from "next/head";
+import React from "react";
+import Header from "./Header";
+import Search from "./Search";
 
-function Layout({title, keywords, description, children}) {
+function Layout({ title, keywords, description, children }) {
   return (
     <div>
-        <Head>
-            <title>{title}</title>
-            <meta name='keywords' content={keywords} />
-            <meta name='description' content={description} />
-        </Head>
-        <Header />
-        <main className='container mx-auto my-7'>{children}</main>
-        <link rel='icon' href='/favicon.ico' />
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+      </Head>
+      <Header />
+      <Search />
+      <main className="container mx-auto my-7">{children}</main>
+      <link rel="icon" href="/favicon.ico" />
     </div>
-  )
+  );
 }
 
 Layout.defaultProps = {
-    title: 'Welcome to Devspace',
-    keywords: 'development, coding, programming',
-    description: 'Latest information in development'
-}
+  title: "Welcome to Devspace",
+  keywords: "development, coding, programming",
+  description: "Latest information in development",
+};
 
-export default Layout
+export default Layout;
